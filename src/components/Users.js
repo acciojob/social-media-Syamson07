cba5297df53c5fca54296e80779a92f951a8170e
Syam Son Penumala <syamson132000@gmail.com>
2025-06-08 19:56:23 +0530

    Create Users.js

---
+++ src/components/Users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Users = () => {
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
