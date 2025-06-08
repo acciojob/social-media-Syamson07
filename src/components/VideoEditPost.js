b9e22904c04641e4e2179c200a3881ae63f9f381
Syam Son Penumala <syamson132000@gmail.com>
2025-06-08 19:56:54 +0530

    Create VideoEditPost.js

---
+++ src/components/VideoEditPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ViewEditPost = () => {
+  return (
+    <div>
+      <h2>Posts</h2>
+      <div className="post">Example Post</div>
+    </div>
+  );
+};
+
+export default ViewEditPost;
