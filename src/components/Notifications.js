d0840884ca836fd3d309c6fc1127982ed182c282
Syam Son Penumala <syamson132000@gmail.com>
2025-06-08 19:56:00 +0530

    Create Notifications.js

---
+++ src/components/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notifications = () => {
+  return (
+    <div>
+      <h2>Notifications</h2>
+      <button className="button">Clear All</button>
+    </div>
+  );
+};
+
+export default Notifications;
